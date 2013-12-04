1f7974dd4d096859cdd44378b53596b93f8b1786
Nícolas Iensen <nicolas@iensen.me>
2013-12-04 17:13:50 -0200

    [#61855940] add Sendgrid support

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,7 @@ class UserMailer < ActionMailer::Base
   default from: "Meu Rio <contato@meurio.org.br>"
 
   def welcome user
+    headers "X-SMTPAPI" => "{ \"category\": [\"vds\", \"welcome\"] }"
     mail(to: user.email, subject: "Verão do Saneamento")
   end
 end
