098b113bbbaafb2e6246f4919aefad8299716356
Nícolas Iensen <nicolas@iensen.me>
2013-12-04 09:25:47 -0200

    [#61855940] Rails template and user resource

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VeraoDoSaneamento::Application.config.secret_key_base = 'c2d066882b617218bb77caa8d55e2aadfcc0db38ed2170f26ba8fd0d1d5c6b7e0c7fbff0bf77033618f49c60170d72f0fd6b0e3099cd1c79c6945058a0accc51'
